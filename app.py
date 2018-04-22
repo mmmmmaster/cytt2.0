7ee67c0598229ed88063911e682078e7323645af
刘杰 <1343451123@qq.com>
2018-04-22 23:28:15 +0800

    添加数据库连接

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import sys, os
 reload(sys)
 sys.setdefaultencoding("utf8")
 
+import motor
 from tornado.web import Application, StaticFileHandler
 from tornado.ioloop import IOLoop
-
+import setting
 sys.path.append(os.path.join(os.path.dirname(__file__), "py"))#把py加入系统路径
 from index import IndexHandler
 
@@ -14,7 +15,8 @@ def main():
   settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "dist/apps"),#静态资源路径
     "template_path": os.path.join(os.path.dirname(__file__), "dist/apps"),#render的路径
-    "debug": True
+    "debug": True,
+    "db":motor.MotorClient(setting.CMS_MONGO_STRING).test
   }
   handlers = [(r"/", IndexHandler),
               (r"/dist/apps/(.*)", StaticFileHandler, {"path": settings["static_path"]})]
--- py/index.py
+++ py/index.py
@@ -1,7 +1,14 @@
 from tornado.web import RequestHandler
+from tornado.gen import coroutine
 
 
 class IndexHandler(RequestHandler):
+  @property
+  def db(self):
+    return self.settings["db"]
+
+  @coroutine
   def get(self):
-    self.write("hello")
+    result = yield self.db.test2.find_one()
+    print result
     self.render("shouye/index.html")
